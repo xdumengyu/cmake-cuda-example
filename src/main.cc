2d732351f8418d0cdf1652f49f22e5f7bbdbc16a
xdumengyu <xdumengyu@gmail.com>
2018-03-30 20:01:05 +0800

    add opencv link library

--- src/main.cc
+++ src/main.cc
@@ -3,7 +3,9 @@
 #ifdef USE_CUDA
 #include "gpu.hpp"
 #endif
-
+#include <opencv2/opencv.hpp>
+using namespace std;
+using namespace cv;
 int main()
 {
     std::cout << "Hello, world!" << std::endl;
@@ -14,6 +16,11 @@ int main()
 #else
     std::cout << "CUDA: Off" << std::endl;
 #endif
-
+	Mat seed = imread("seeds.png", 0);
+	if (seed.empty())
+	{
+		cout << "error reading image, get NULL" << endl;
+	}
+	waitKey();
     return 0;
 }
